f6279441cd364e2dfcb8a93bc57d7358b317377b
qishilong <663253062@qq.com>
2024-08-04 16:10:52 +0800

    ci: 继续完善第一个 flow

--- components/first-flow/index.tsx
+++ components/first-flow/index.tsx
@@ -1,18 +1,54 @@
-import React from "react";
-import { ReactFlow } from "@xyflow/react";
+"use client";
+
+import type { FC } from "react";
+import { useCallback } from "react";
+import {
+  ReactFlow,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  MiniMap,
+  Controls,
+  Background,
+} from "@xyflow/react";
 
 import "@xyflow/react/dist/style.css";
 
 const initialNodes = [
   { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } }
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
 ];
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
-export default function FirstFlow() {
+const FirstFlow: FC = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: any) => {
+      setEdges((eds) => addEdge(params, eds));
+    },
+    [setEdges]
+  );
+
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <MiniMap />
+        <Background
+          gap={12}
+          size={1}
+        />
+      </ReactFlow>
     </div>
   );
-}
+};
+
+export default FirstFlow;
